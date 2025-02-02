e2d86ea656106e4d7201d02a44553d9ee056c4f3
Sathish Kumar <sathishc58@gmail.com>
2025-02-02 15:41:00 +0530

    Added another text field

--- gui.py
+++ gui.py
@@ -5,8 +5,10 @@ window = Tk()
 window.title("Welcome to Python GUI")
 
 lblFName = Label(window, text="First Name", font=("Verdana", 15))
+lblLName = Label(window, text="Last Name", font=("Verdana", 15))
 
 txtFName = Entry(window)
+txtLName = Entry(window)
 
 
 btnValidate = Button(window, text="Validate")
@@ -14,8 +16,10 @@ btnValidate = Button(window, text="Validate")
 lblFName.grid(column=0, row=0)
 txtFName.grid(column=1, row=0)
 
+lblLName.grid(column=0, row=1)
+txtLName.grid(column=1, row=1)
 
-btnValidate.grid(column=0, row=1)
+btnValidate.grid(column=0, row=2)
 
 txtFName.focus()
 
